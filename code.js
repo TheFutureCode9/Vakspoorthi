400f1d20710784dba6a07cd6d629f2c18284fb8f
TheFutureCode <94366543+TheFutureCode9@users.noreply.github.com>
2022-10-27 16:42:19 +0530

    Update code.js

--- code.js
+++ code.js
@@ -50,51 +50,58 @@ function gotResults(error, results) {
 
 
       
-Webcam.attach( '#camera' );
 
 camera = document.getElementById("camera");
       
-  Webcam.set({
-    width:350,
-    height:300,
-    image_format : 'png',
-    png_quality:90
-  });
-
+Webcam.set({
+  width:350,
+  height:300,
+  image_format : 'png',
+  png_quality:90
+});
+Webcam.attach( '#camera' );
 function take_snapshot()
 {
-    Webcam.snap(function(data_uri) {
-        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
-    });
+  Webcam.snap(function(data_uri) {
+      document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+  });
 }
 
-  console.log('ml5 version:', ml5.version);
-  
-  // Initialize the Image Classifier method with MobileNet
-//classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/JACkTMciY/model.json',modelLoaded);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/3p1zcmVdQ/model.json',modelLoaded);
+console.log('ml5 version:', ml5.version);
 
-  // When the model is loaded
-  function modelLoaded() {
-    console.log('Model Loaded!');
-  }
-      
-  function check()
-  {
-    img = document.getElementById('selfie_image');
-    classifier.classify(img, gotResult);
-  }
+// Initialize the Image Classifier method with MobileNet
+//classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/JACkTMciY/model.json',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/gAFE2b2NL/model.json',modelLoaded);
+
+// When the model is loaded
+function modelLoaded() {
+  console.log('Model Loaded!');
+}
+    
+function check()
+{
+  img = document.getElementById('selfie_image');
+  classifier.classify(img, gotResult);
+}
 
 
 // A function to run when we get any errors and the results
-function gotResult(error, results) {
-  // Display error in the console
-  if (error) {
-    console.error(error);
-  } else {
-    // The results are in an array ordered by confidence.
-    console.log(results);
-    document.getElementById("result_object_name").innerHTML = results[0].label;
-    document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
-  }
+function gotResult(error, result) {
+// Display error in the console
+if (error) {
+  console.error(error);
+} else {
+  // The results are in an array ordered by confidence.
+  console.log(result);
+  document.getElementById("result_object_name").innerHTML = result[0].label;
+  document.getElementById("result_object_accuracy").innerHTML = result[0].confidence.toFixed(3);
 }
+}
+
+function back()
+{
+    window.location="index.html";
+}
+
+    
+
